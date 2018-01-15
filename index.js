25d501ecc4f8d243eedf63b0900ca62d837b2386
David Melo <davidmelo@desmdesigns.com>
2018-01-14 20:42:36 -0500

    Fixes Typo With vinyl

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var path = require('path');
 
 // Utility functions for gulp plugins.
 // @see https://www.npmjs.com/package/gulp-util
-var vynil = require('vynil');
+var vinyl = require('vinyl');
 var err = require('plugin-error');
 
 // Construct pipes of streams of events.
@@ -29,13 +29,13 @@ var noop = Function.prototype;
 // The PluginError object represents an error when a value is not of the expected type.
 // @see https://www.npmjs.com/package/gulp-util#new-pluginerrorpluginname-message-options
 function createError(message) {
-  return new vynil.PluginError('gulp-requirerc', message, { showStack: true });
+  return new err.PluginError('gulp-requirerc', message, { showStack: true });
 }
 
 // Method that creates a virtual file format.
 // @see https://www.npmjs.com/package/gulp-util#new-fileobj
 function createFile(filename, output, buildResponse, sourceMap) {
-  var newFile = new vynil.File({ path: filename, contents: new Buffer(output) });
+  var newFile = new vinyl.File({ path: filename, contents: new Buffer(output) });
   newFile.buildResponse = buildResponse.replace('FUNCTION', filename);
   if (sourceMap) newFile.sourceMap = JSON.parse(sourceMap);
   return newFile;
